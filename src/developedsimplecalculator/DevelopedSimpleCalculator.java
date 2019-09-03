55d1f4f109c89b06ac4eb766d96046cdcffd280f
isuriimalsha <isuriimalsha94@gmail.com>
2019-09-03 11:19:35 +0530

    First commit

---
+++ src/developedsimplecalculator/DevelopedSimpleCalculator.java
@@ -0,0 +1,16 @@
+
+package developedsimplecalculator;
+
+/**
+ *
+ * @author Isuri Imalsha
+ */
+public class DevelopedSimpleCalculator {
+
+    public static void main(String[] args) {
+        Calculator c  = new Calculator();
+        c.setVisible(true);
+    
+    }
+    
+}
